2335217468a35a4f45963a58bbc7c9eeb5fb5805
uliana5858 <118607939+uliana5858@users.noreply.github.com>
2022-12-04 18:23:20 +0900

    added Rrog

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!");
+﻿Console.Write("Enter your name:");
+string username = Console.ReadLine();
+Console.Write("Hi,");
+Console.Write(username);
